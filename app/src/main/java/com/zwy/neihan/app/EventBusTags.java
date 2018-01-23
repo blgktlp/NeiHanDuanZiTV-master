ab3525a351af28bd1e73d49db40e6e27762b59fa
blgktlp <1426321596@qq.com>
2018-01-23 20:48:42 +0800

    内涵

--- app/src/main/java/com/zwy/neihan/app/EventBusTags.java
+++ app/src/main/java/com/zwy/neihan/app/EventBusTags.java
@@ -4,7 +4,7 @@ package com.zwy.neihan.app;
  * Created by jess on 8/30/16 16:39
  * Contact with jess.yan.effort@gmail.com
  */
-//1234567890111
+//123456789011111
 public interface EventBusTags {
     /*取二维码/条形码扫描结果*/
     public static final String SCAN_QR_CODE_RESULT = "scan_qr_code_result";
